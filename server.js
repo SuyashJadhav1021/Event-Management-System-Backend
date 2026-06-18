f13b1e6b5116cefe6c4e00bf2c6eb4fb24cc0bf7
SuyashJadhav1021 <suyashjadhav2831@gmail.com>
2026-06-18 16:25:14 +0530

    changes

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const errorHandler = require("./middleware/errorHandler");
 dotenv.config();
 
 // Connect to MongoDB
-console.log("MONGO_URI:", process.env.MONGO_URI);
 connectDB();
 
 const app = express();
@@ -42,10 +41,12 @@ app.use((req, res) => {
 
 // ── Central Error Handler (must be last) ─────────────────────────────────────
 app.use(errorHandler);
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(
-    `🚀 Server running on port ${PORT} in ${process.env.NODE_ENV || "development"} mode`,
-  );
-});
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(` Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
