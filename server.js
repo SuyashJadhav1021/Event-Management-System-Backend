3f427110485acb7409bddd2081e9bd17ea582446
SuyashJadhav1021 <suyashjadhav2831@gmail.com>
2026-06-18 17:23:31 +0530

    changes

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ connectDB();
 const app = express();
 
 // Middleware
-
 const allowedOrigins = [
   "http://localhost:3000",
   "https://event-management-system-frontend-hz.vercel.app",
@@ -19,18 +18,16 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+// IMPORTANT: handle preflight
+app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
